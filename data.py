07901b0bfa6529c120b73a6831fcf88986c593fa
Vivek Sundaray <viveksundaray123@outlook.com>
2022-06-21 21:56:58 +0530

    Made the program production level

---
+++ data.py
@@ -0,0 +1,24 @@
+days = ["Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"]
+
+century_odd_days = {
+    0: 0,
+    100: 5,
+    200: 3,
+    300: 1
+}
+
+month_odd_days = {
+    1: 3,
+    2: [0, 1],
+    3: 3,
+    4: 2,
+    5: 3,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 2,
+    10: 3,
+    11: 2,
+    12: 3
+}
--- main.py
+++ main.py
@@ -1,87 +1,69 @@
-days = ["Monday", "Tuesday", "Wednesday",
-        "Thursday", "Friday", "Saturday", "Sunday"]
-
-odd_days = {
-    0: 0,
-    100: 5,
-    200: 3,
-    300: 1
-}
-
-month_odd_days = {
-    1: 3,
-    2: [0, 1],
-    3: 3,
-    4: 2,
-    5: 3,
-    6: 2,
-    7: 3,
-    8: 3,
-    9: 2,
-    10: 3,
-    11: 2,
-    12: 1
-}
+from data import days, month_odd_days, century_odd_days
 
 
-def find_day(date):
-    date_splitted = date.split("-")
-    day = int(date_splitted[0])
-    month = int(date_splitted[1])
-    year = int(date_splitted[2])
+def find_day(day, month, year):
+    # Restrictions
     if day > 31:
         return "invalid date"
     elif month > 12:
         return "invalid month"
-    elif year < 0:
+    elif year <= 0:
         return "invalid year"
-    odd_days_of_year = odd_days_year(year)
-    if year % 400 == 0:
-        leap_year = True
-    elif year % 100 != 0 and year % 4 == 0:
-        leap_year = True
     else:
-        leap_year = False
-    odd_days_daynmonth = odd_days_of_day_and_month(
-        day, month, leap_year)
-    total_odd_days = odd_days_of_year + odd_days_daynmonth
-    return days[total_odd_days % 7 - 1]
+        # Odd days in year calculation
+        years_completed = year - 1
+        new_year = years_completed % 400
+        century_year = (new_year // 100)*100
+        remaining_year = new_year - century_year
 
+        # Odd days for century
+        for k, v in century_odd_days.items():
+            if century_year == k:
+                odd_days_century_year = v
 
-def odd_days_year(year):
-    years_completed = year - 1
-    new_year = years_completed % 400
-    century_year = (new_year // 100)*100
-    remaining_year = new_year % 100
+        # Leap and non-leap year calculation
+        leap_year_in_remaining_year = remaining_year // 4
+        ordinary_year_in_remaining_year = remaining_year - leap_year_in_remaining_year
 
-    for k, v in odd_days.items():
-        if century_year == k:
-            odd_days_century_year = v
+        # Odd days in leap and non-leap year
+        odd_days_in_leap_year = leap_year_in_remaining_year * 2
+        odd_days_in_ordinary_year = ordinary_year_in_remaining_year * 1
 
-    leap_year_in_remaining_year = remaining_year // 4
-    ordinary_year_in_remaining_year = remaining_year - leap_year_in_remaining_year
-    odd_days_in_leap_year = leap_year_in_remaining_year * 2
-    odd_days_in_ordinary_year = ordinary_year_in_remaining_year * 1
-    total_odd_days = odd_days_century_year + \
-        odd_days_in_leap_year + odd_days_in_ordinary_year
-    return total_odd_days % 7
+        # Total odd days of year and non-leap year
+        total_odd_days_of_year = odd_days_century_year + \
+            odd_days_in_leap_year + odd_days_in_ordinary_year
 
-
-def odd_days_of_day_and_month(day, month, leap_year):
-    odd_days_of_month = 0
-    for k, v in month_odd_days.items():
-        if k == 2:
-            if leap_year:
-                v = month_odd_days[2][1]
-            else:
-                v = month_odd_days[2][0]
-        elif k == month:
-            break
+        # Odd days in day and month calculation
+        # Check if leap year
+        if year % 400 == 0:
+            leap_year = True
+        elif year % 100 != 0 and year % 4 == 0:
+            leap_year = True
         else:
-            odd_days_of_month += v
-            continue
-    total_odd_days = odd_days_of_month + day
-    return total_odd_days % 7
+            leap_year = False
+        odd_days_of_month = 0
+
+        # Odd days in month
+        for k, v in month_odd_days.items():
+            # If the key is equal to the month we don't need to add the odd days
+            if k == month:
+                break
+
+            else:
+                if k == 2:
+                    # If it is a leap year then February has 1 odd day otherwise 0
+                    if leap_year:
+                        v = month_odd_days[2][1]
+                    else:
+                        v = month_odd_days[2][0]
+                odd_days_of_month += v
+        total_odd_days_of_month_and_day = odd_days_of_month + day
+
+        # Final result
+        final_odd_days = (total_odd_days_of_year +
+                          total_odd_days_of_month_and_day) % 7
+        return days[final_odd_days - 1]
 
 
-print(find_day("17-01-2006"))
+if __name__ == "__main__":
+    print(find_day(12, 12, 2012))
